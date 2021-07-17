cbee8fb1577e4b17866865e1f1faad8c630eb43f
fsantander <fsantander@ing.ucsc.cl>
2021-07-17 19:55:53 -0400

    Comentarios varios

--- serverEjemplo/baseDatos.py
+++ serverEjemplo/baseDatos.py
@@ -9,12 +9,20 @@ class baseDatos:
         con = sqlite3.connect("pokeBaseDatos.db")
         cur = con.cursor()
         
-        for row in cur.execute('SELECT * FROM pokemon WHERE id='+str(idPokemon)):            
+        for row in cur.execute('SELECT * FROM pokemon WHERE id='+str(idPokemon)):
+            #imprime la linea completa            
             print(row)
+            #imprime el elemento en la posición 0
             print(row[0])
+            #se supone que creo un json
             data_set = {'id':str(row[0]),'nombre':str(row[1]),'total': str(row[3]),'hp':str(row[4]),'attack':str(row[5]),'defense':str(row[6]),'specialAttack':str(row[7]),'specialDefense':str(row[8]),'speed':str(row[9]),'generation':str(row[10])}
+            #podría devolver esto y era
             json_dump = json.dumps(data_set)
             print(json_dump)
+            #pruba para pasar el json a un array de python
             y = json.loads(json_dump)
+            #creo que se crea un diccionario, supongoque de inicio podría crear uno  ¯\_(ツ)_/¯
+            #imprimir el elemento que responde a nombre
             print(y["nombre"])
+            #ya acá retorno el objeto para devolverlo al cliente
             return json_dump
