d924470b2481f6afb5762717a37f0a341b5a002d
jchamorro <jchamorro@ing.ucsc.cl>
2021-07-24 19:15:05 -0400

    poke

--- clientEjemplo/forms.py
+++ clientEjemplo/forms.py
@@ -1,7 +1,6 @@
 from wtforms import Form
-
-from wtforms import DecimalField, IntegerField, StringField, FieldList, TextField
-
+from wtforms import DecimalField, IntegerField, StringField, FieldList, TextField, SelectField
+from flask import request
 class HelloForm(Form):
     """docstring for HelloForm."""
     name = StringField('name')
@@ -19,3 +18,22 @@ class ListHelloForm(Form):
 class consultaPokemon(Form):
     """docstring for SumForm."""
     pokemon = IntegerField('pokemon')
+
+class pokeHoroscopoForm(Form):
+    anho = IntegerField('año')
+
+def pokeBatallaForm(listaPokemonActualizada):
+    class MyForm(Form):
+        pokemonIngresado = SelectField(u'Selecciona tu Pokemon', choices = listaPokemonActualizada)
+    return MyForm(request.form) 
+
+#class pokeBatallaForm2(Form):
+ #   listaPokemon=["EEVEE"]
+  #  def actualizaLista(self, listaActualizada):
+   #     self.listaPokemon = listaActualizada
+
+    #pokemonIngresado = SelectField(u'Selecciona tu Pokemon', choices = listaPokemon)
+
+
+
+
